4276be2ea50fa5421f08130c17d25f4acae45a95
hmtmcse <hmtm.cse@gmail.com>
2022-02-27 15:29:38 +0600

    update message

--- pf_flask_file/pfff_message.py
+++ pf_flask_file/pfff_message.py
@@ -1,5 +1,5 @@
 class PFFFMessage:
     FILE_SIZE_NOT_MATCH = "File size is bigger than allowed"
-    INVALID_EXTENSION = "Invalid file extension"
+    INVALID_EXTENSION = "Invalid uploaded file"
     VALIDATION_ERROR = "Validation errors"
-    INVALID_UPLOAD_PATH = "Invalid upload paht"
+    INVALID_UPLOAD_PATH = "Invalid upload path"
